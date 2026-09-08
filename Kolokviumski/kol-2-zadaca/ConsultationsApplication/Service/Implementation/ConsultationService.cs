03182b3f61be5f81d09aff47c3aa917832ea3a61
TaseZmaj <stefan.tasevski.2@students.finki.ukim.mk>
2026-09-08 21:52:56 +0200

    Added the starter code for kol-2

---
+++ Kolokviumski/kol-2-zadaca/ConsultationsApplication/Service/Implementation/ConsultationService.cs
@@ -0,0 +1,109 @@
+using Domain.Dto;
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Repository.Interface;
+using Service.Interface;
+
+namespace Service.Implementation;
+
+public class ConsultationService : IConsultationService
+{
+    private readonly IRepository<Consultation> _consultationRepository;
+
+    public ConsultationService(IRepository<Consultation> consultationRepository)
+    {
+        _consultationRepository = consultationRepository;
+    }
+
+    public async Task<Consultation> GetByIdNotNullAsync(Guid id)
+    {
+        var result = await GetByIdAsync(id);
+
+        if (result == null)
+        {
+            throw new InvalidOperationException($"Consultation with id {id} not found");
+        }
+
+        return result;
+    }
+
+    public async Task<Consultation?> GetByIdAsync(Guid id)
+    {
+        return await _consultationRepository.GetAsync(
+            selector: x => x,
+            predicate: x => x.Id == id);
+    }
+
+    public async Task<List<Consultation>> GetAllAsync(string? roomName, DateOnly? date)
+    {
+        return await _consultationRepository.GetAllAsync(
+            selector: x => x,
+            predicate: x => (roomName == null || x.Room.Name.Contains(roomName)) &&
+                (date == null || DateOnly.FromDateTime(x.StartTime) == date),
+            include: x => x.Include(c => c.Attendances).ThenInclude(a => a.User));
+    }
+
+    public async Task<Consultation> CreateAsync(DateTime startTime, DateTime endTime, Guid roomId)
+    {
+        var consultation = new Consultation()
+        {
+            StartTime = startTime,
+            EndTime = endTime,
+            RoomId = roomId,
+            RegisteredStudents = 0
+        };
+
+        return await _consultationRepository.InsertAsync(consultation);
+    }
+
+    public async Task<Consultation> UpdateAsync(Guid id, DateTime startTime, DateTime endTime, Guid roomId)
+    {
+        var consultation = await GetByIdNotNullAsync(id);
+
+        if (consultation.RegisteredStudents > 0)
+        {
+            throw new InvalidOperationException($"Consultation with id {id} already has registered students");
+        }
+        
+        consultation.StartTime = startTime;
+        consultation.EndTime = endTime;
+        consultation.RoomId = roomId;
+        return await _consultationRepository.UpdateAsync(consultation);
+    }
+
+    public async Task<Consultation> DeleteByIdAsync(Guid id)
+    {
+        var consultation = await GetByIdNotNullAsync(id);
+        
+        if (consultation.RegisteredStudents > 0)
+        {
+            throw new InvalidOperationException($"Consultation with id {id} already has registered students");
+        }
+        
+        return await _consultationRepository.DeleteAsync(consultation);
+    }
+
+    public async Task<PaginatedResult<Consultation>> GetPagedAsync(int pageNumber, int pageSize)
+    {
+        return await _consultationRepository.GetAllPagedAsync(
+            selector: x => x,
+            include: x=> x.Include(c => c.Attendances).ThenInclude(a => a.User),
+            pageNumber: pageNumber,
+            pageSize: pageSize,
+            asNoTracking: true);
+    }
+
+    public async Task IncrementNumberOfStudentsAsync(Guid id)
+    {
+        var consultation = await GetByIdNotNullAsync(id);
+        consultation.RegisteredStudents += 1;
+        await _consultationRepository.UpdateAsync(consultation);
+    }
+    
+    public async Task DecrementNumberOfStudentsAsync(Guid id)
+    {
+        var consultation = await GetByIdNotNullAsync(id);
+        consultation.RegisteredStudents -= 1;
+        await _consultationRepository.UpdateAsync(consultation);
+    }
+}
